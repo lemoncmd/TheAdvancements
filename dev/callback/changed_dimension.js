fdd06e209db8c60c99966c012f1309c95d3b0b3d
lemoncmd <lemoncmd@gmail.com>
2018-04-05 22:16:17 +0900

    bug fix
    dimension bug fix

--- dev/callback/changed_dimension.js
+++ dev/callback/changed_dimension.js
@@ -3,14 +3,16 @@ var nowDim = -1;
 
 
 Callback.addCallback("DimensionLoaded", function(id){
-    if(dimension[id]){
-        if(~nowDim){
-            api.invokeCriterias("changed_dimension", function(cond){
-                return (!cond.from || cond.from == dimension[nowDim]) && (!cond.to || cond.to == dimension[id]);
-            });
-        }
-        nowDim = dimension[id];
+    if(~nowDim){
+        api.invokeCriterias("changed_dimension", function(cond){
+            return (!cond.from ||cond.from == dimension[nowDim]) && (!cond.to || cond.to == dimension[id]);
+        });
     }
+    nowDim = id;
+});
+
+Callback.addCallback("LevelLoaded", function () {
+    nowDim = Player.getDimension();
 });
 
 Callback.addCallback("LevelLeft", function () {
